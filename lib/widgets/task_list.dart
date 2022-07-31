7a0b675cc4fa1cb5734df2c29233f12a9a7ec2d2
Zoir I <wowdobryy@gmail.com>
2022-07-31 17:08:47 +0100

    created models/widgets

---
+++ lib/widgets/task_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:tracker/screens/task_tile.dart';
+
+class TaskList extends StatefulWidget {
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+
+  List<Task> tasks = [
+
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        TaskTile(taskTitle: tasks[0].name, isChecked: tasks[0].isDone,),
+        TaskTile(taskTitle: tasks[1].name, isChecked: tasks[1].isDone,),
+        TaskTile(taskTitle: tasks[2].name, isChecked: tasks[2].isDone,),
+      ],
+    );
+  }
+}
